67ebd871d1c913ec0c34f19778b54ce181b4368e
Kgomotso Welcome <kgomotsowelcome02@gmail.com>
2019-04-22 05:58:46 +0200

    Created Makefile and added to git

--- src/Barrier.java
+++ src/Barrier.java
@@ -4,44 +4,40 @@ import java.util.concurrent.Semaphore;
 public class Barrier {
 	
 	// add missing variables
-	int size;
+	int size; // number of threads
+	int counter = 0; // variable that will be accessed by all threads, checks number of threads at barrier. 
+	int held = 1; 
+	int unheld = 0;
+
+	public Semaphore arrival = new Semaphore(held);
+    public Semaphore departure = new Semaphore(unheld);
 	
 	Barrier(int n) {
 		// complete this constructor
 		this.size = n;	
 	}
-	public Semaphore arrival = new Semaphore(1);
-    public Semaphore departure = new Semaphore(0);
-	int counter = 0;
+	
 	
 	public void b_wait() throws InterruptedException{
 		// this is the only additional method you will need to complete
-		/*
-		arrival.acquire();
-		counter++;
-		arrival.release();
 
-		if(counter == size){
-			departure.release();
-		}
-
-		departure.acquire();
-		departure.release();
-		*/
-		
-        arrival.acquire();
-        counter++;
-        if (counter < size) {
-            arrival.release();
+		// acquire arrival and set arrival to o 
+        arrival.acquire(); // arrival = 0, blocks other threads from accessing counter
+        // modify counter 
+        counter++; // critical section 
+        // check if counter is less than number of theads
+        if (counter < size) { 
+            arrival.release(); // release counter after modification. Notify other threads.
         }else{
-            departure.release();
+        	// line will be executed if counter > size 
+        	// should be executed once final thread has modified counter. 
+            departure.release(); 
         }
 
-        departure.acquire();
-        counter--;
+        departure.acquire(); 
+        counter--; // critical section 
         if (counter > 0) {
             departure.release();
 	    }
 	}
-
 }
